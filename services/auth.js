47dd1c9d1bb2c07aed5fc8b06d543056753d71da
Vedant-Sagwal <vedantsagwal@gmail.com>
2025-07-25 21:49:11 +0530

    Some Minor changes

--- services/auth.js
+++ services/auth.js
@@ -1,27 +1,27 @@
-const jwt=require("jsonwebtoken")
-const SecretKey="StupidFuck"
+const jwt = require("jsonwebtoken")
+const SecretKey = ""//secret key
 function setUser(user) {
-    if(!user) return res.send("no user");
-    return jwt.sign({
-        _id:user._id,
-        name:user.name,
-        email:user.email,
-        password:user.password,
-        cycleNum:user.cycleNum
-    },SecretKey)
-    
-} 
+  if (!user) return res.send("no user");
+  return jwt.sign({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    password: user.password,
+    cycleNum: user.cycleNum
+  }, SecretKey)
+
+}
 
 async function getUser(token) {
-    if(!token) return null
-    try {
-        return jwt.verify(token,SecretKey);
-    } catch (error) {
-        return null;
-    }
+  if (!token) return null
+  try {
+    return jwt.verify(token, SecretKey);
+  } catch (error) {
+    return null;
+  }
 }
 
 module.exports = {
-    getUser,
-    setUser,
+  getUser,
+  setUser,
 }
